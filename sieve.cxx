5e52079a8830d2983c84ff5c0beafc6a31bcdade
fen-ton <31051194+fen-ton@users.noreply.github.com>
2018-06-02 10:17:53 +0100

    added sieve.cxx - prime sieve

---
+++ sieve.cxx
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() 
+{
+    const int N = 37;
+    constexpr int SIZE = N+1;
+    int* sieve {new int[SIZE]};
+    for(auto i=1; i<SIZE; ++i)
+        sieve[i] = i;
+    for(auto i=2; i<SIZE/2; ++i)
+        for(auto j=2*i; j<N+1; j+=i)
+            sieve[j] = 0;
+    for(auto i=1; i<SIZE; ++i)
+        if(sieve[i] != 0)
+            std::cout<<sieve[i]<<"\n";
+    return 0;
+}
